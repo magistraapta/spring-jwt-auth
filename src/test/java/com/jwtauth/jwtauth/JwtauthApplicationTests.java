1c8ce5380082df165020a0ee8ecf9aeb7e53a990
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-07-29 12:28:11 +0800

    fix: test

--- src/test/java/com/jwtauth/jwtauth/JwtauthApplicationTests.java
+++ src/test/java/com/jwtauth/jwtauth/JwtauthApplicationTests.java
@@ -2,8 +2,10 @@ package com.jwtauth.jwtauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JwtauthApplicationTests {
 
 	@Test
